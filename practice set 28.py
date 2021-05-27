c1c4df9d26e1e592578327b196d0a24108b2ebe6
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-05-27 11:58:42 +0530

    maximum and minimum of three no.s

---
+++ practice set 28.py
@@ -0,0 +1,20 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+d=max(a,b,c)
+e=min(a,b,c)
+print(d)
+print(e)
+
+#---------------------------------------#
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+if(a>b):
+    print(a)
+elif(b>c):
+    print(b)
+else:
+    print(c)
+
