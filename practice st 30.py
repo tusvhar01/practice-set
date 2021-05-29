48291157cd6f34159297d72c683d0be843c3596a
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-05-29 18:50:59 +0530

    Enter secret code to get out from loop

---
+++ practice st 30.py
@@ -0,0 +1,10 @@
+secret_number=777
+n=int(input("Enter a no.: "))
+while(n!=777):
+    if(n!=secret_number):
+        print("Ha ha! You're stuck in my loop!")
+    n=int(input("Enter a no.: "))
+print("Well done, muggle! You are free now.")
+
+
+# Game , To choose a secret no.
