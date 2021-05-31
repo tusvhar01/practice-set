16a998aefe769f1c6da06069be8e820a4459ebf6
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-05-31 12:25:36 +0530

    height of pyramid by entering number of block

---
+++ practice set 31.py
@@ -0,0 +1,13 @@
+blocks = int(input("Enter the number of blocks: "))
+height=0
+layer=1
+while(layer<=blocks):
+    blocks=blocks-layer
+    layer=layer+1
+    height=height+1
+print("The height of the pyramid:", height)
+
+
+
+## height of blocks
+
