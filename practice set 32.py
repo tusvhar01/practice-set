5f85b478c0070ced7ebd363d3c2519966524c8bc
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-06-01 22:16:12 +0530

    Enter a secret word to get out from loop

---
+++ practice set 32.py
@@ -0,0 +1,11 @@
+m="chupacabra"
+n=input()
+while True:
+    if (n==m):
+        print("You've successfully left the loop.")
+        break
+    else:
+        n=input()
+
+        # enter a secret code to exit a loop
+
