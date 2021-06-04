ca7ab0a8ad85241b02d384222c4d0005804195b6
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-06-04 20:46:18 +0530

    Basic commands of List in Python

---
+++ practice set 35.py
@@ -0,0 +1,12 @@
+n=[1,3,4,56,7]
+n.insert(0,0)
+print(n)
+print('length',len(n))
+n.append(99)
+print(n)                        ## Some basic command in list
+print("length=",len(n))
+n[3]=77
+print(n)
+print("length=",len(n))
+n.sort()
+print("sorted",n)
