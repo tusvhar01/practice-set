693ee9c5a043bd5a496f3a6fcaf43a37b2adb42f
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-06-10 15:44:54 +0530

    Basics of dictionary

---
+++ Practice set 39.py
@@ -0,0 +1,20 @@
+a={
+    "Tushar": "want_to_get_job",
+    "vishal": "want_to_qualify_neet",
+    "om": "to_be_Good_in_maths"
+}
+
+a['Rajeev']='get_government_job'            #basics of dictionary
+print(a)
+
+
+dic={"9":"asd","8":"qwe"}
+
+dic["9"]="poi"
+dic["7"]="Tus"
+dic.items()
+print(dic)
+dic.values()
+print(dic)
+dic.keys()
+print(dic)
