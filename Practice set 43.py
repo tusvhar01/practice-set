8f2b90c839a1cdf13bd40ac596cb594b2ac3a208
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-06-17 17:35:46 +0530

    Basics of dictionary

---
+++ Practice set 43.py
@@ -0,0 +1,48 @@
+'''pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+for key, value in pol_eng_dictionary.items():
+    print("Pol/Eng ->", key, ":", value)
+
+
+
+pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+print(len(pol_eng_dictionary))    # outputs: 3
+del pol_eng_dictionary["zamek"]    # remove an item
+print(len(pol_eng_dictionary))    # outputs: 2
+
+pol_eng_dictionary.clear()   # removes all the items
+print(len(pol_eng_dictionary))    # outputs: 0
+
+del pol_eng_dictionary    # removes the dictionary
+
+
+'''
+
+pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+for key, value in pol_eng_dictionary.items():
+    print(key,":",value)
+
+
+
+
+
+
+
+
+
+
+
