8b542590ef10aac041eb70d2100461842792c60b
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-06-18 11:28:52 +0530

    To count to duplicates in tuple

---
+++ Practice set 44.py
@@ -0,0 +1,7 @@
+tup = (1, 2, 3, 2, 4, 5, 6, 2, 7, 2, 8, 9)
+duplicates = tup.count(2)
+
+print("Total dublicates are: ",duplicates)
+
+
+                            # To count to duplicates in tuple
