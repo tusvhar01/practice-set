28dbc584952b2e43d82dec1f042bd05bd26f45ab
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-06-22 00:21:28 +0530

    user inputed must be in str.
    cat and hat should be in str so it give true otherwise false and no.of cat and hat should be in same

---
+++ practice set 45.py
@@ -0,0 +1,8 @@
+def cat_hat(str):
+    return str.count('cat')==str.count('hat')
+print(cat_hat(input()))
+
+
+
+
+                            #cat and hat should be in str so it give true otherwise false and no.of cat and hat should be in same
