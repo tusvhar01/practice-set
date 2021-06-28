fb738b49077784126e6a75e8f9c82b0570ccfd74
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-06-28 23:17:51 +0530

    Even no. in range between two no.s inputed

---
+++ Practice set 52.py
@@ -0,0 +1,6 @@
+#x,y=int(input().split(","))
+x,y=eval(input())
+for i in range(x,y+1):
+    if(i%2==0):
+        print(i,end=" ")
+                        # Even no. in range between two no.s inputed
