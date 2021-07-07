66db0d26736e0b9198aef01ab025f98000d9f221
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-07-07 23:35:00 +0530

    To know the ASCII or UNICODE of character

---
+++ Practice set 59.py
@@ -0,0 +1,12 @@
+name1='a'
+name2=" "
+name3="A"
+name4='α'                           # greek alpha
+name5='ę'                           # Polish alphabet
+name6="0"                           # numeric
+print(ord(name1))
+print(ord(name2))
+print(ord(name3))                   # To know the ASCII or UNICODE of character
+print(ord(name4))
+print(ord(name5))
+print(ord(name6))
