439809e833412a578542ed1f57f19735dc372c09
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-07-10 23:31:18 +0530

    Entering ascii value and output is Acsii Character

---
+++ Practice set 60.py
@@ -0,0 +1,7 @@
+print(chr(97))
+print(chr(65))
+print(chr(32))                      # space
+print(chr(68))
+print(chr(945))
+print(chr(34))
+                                # Entering ascii value and output is Acsii Character
