9e80c03b421d02f60cfee6cec7e0abd2a793aabc
Tushar Verma <82023765+tusvhar01@users.noreply.github.com>
2021-07-11 23:52:13 +0530

    Indixing string

---
+++ Practice set 61.py
@@ -0,0 +1,19 @@
+# Indexing strings.
+
+the_string = 'silly walks'
+
+for i in range(len(the_string)):
+    print(the_string[i], end=' ')
+
+print()
+
+
+
+the_string = 'silly walks'
+
+for i in the_string:
+    print(i, end=' ')
+
+print()
+
+
